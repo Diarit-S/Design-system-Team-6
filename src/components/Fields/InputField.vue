0920d0c1e49b7f711e7188ea9560016c69395f0f
Diarit <diarit-94@hotmail.fr>
2021-05-20 18:02:10 +0200

    configuration

---
+++ src/components/Fields/InputField.vue
@@ -0,0 +1,38 @@
+<template>
+  <input :type="type" :name="name" class="common-input-field">
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    name: {
+      type: String,
+      default: "Field"
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/vars';
+@import '@/styles/colors';
+
+.common-input-field {
+  padding: 20px 16px;
+  border-radius: $field-border-radius;
+  margin: $field-margin;
+  border-color: $brand-primary-lighter;
+  border-style: solid;
+
+  &:focus {
+    outline: none;
+    border-color: $brand-primary-initial;
+  }
+}
+</style>
---
+++ src/components/FormElementGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-element-group">
+    <label :for="name">
+      {{ label }}
+    </label>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormElementGroup',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form-element-group {
+  label {
+    display: block;
+  }
+}
+</style>
